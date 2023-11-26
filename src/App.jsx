f25f4d585c18b0b4a4864167135535327f2725b7
saitamason <92246829+saitamason@users.noreply.github.com>
2023-11-26 21:10:23 +0100

    feat: add global styles, button, and input

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,25 @@
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles.js";
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+  color: orangered;
+`;
+
+const StyledApp = styled.div`
+  background: green;
+`;
+
 function App() {
-  return <div>Hello world!</div>;
+  return (
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <H1>Hello world!</H1>
+      </StyledApp>
+    </>
+  );
 }
 
 export default App;
